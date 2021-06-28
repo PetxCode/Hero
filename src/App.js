78267eb92b0f0a97df34ba0cd35611f14d0444a6
codelabite <chukwusahpeter@gmail.com>
2021-06-28 20:39:48 +0100

    header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { Fragmanet } from "react";
 import HeroSection from "./component/HeroSection";
 import Footer from "./component/Footer";
+import HeaderComponent from "./component/HeaderComponent";
+import { BrowserRouter, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <HeaderComponent />
+        </Switch>
+      </BrowserRouter>
       <HeroSection />
       <Footer />
-    </div>
+    </>
   );
 };
 
 export default App;
+//
+
+// <BrowserRouter>
+// <Switch>
+// <HeaderComponent />
+
+// </Switch>
+// </BrowserRouter>
